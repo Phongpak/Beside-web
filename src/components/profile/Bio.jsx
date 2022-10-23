1bb5649f689e5438bdddfe5093dda642ca1cfdd3
teepakorn326 <bosskero326@gmail.com>
2022-10-23 18:32:25 +0700

    555

--- src/components/profile/Bio.jsx
+++ src/components/profile/Bio.jsx
@@ -1,7 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import ModalWallet from "../modals/ModalWallet";
+import { useState } from "react";
+import ModalAvailable from "../modals/ModalAvailable";
 
 function Bio() {
+  const [isOpenModalWallet, setIsOpenModalWallet] = useState(false);
+  const [isOpenModalAvailable, setIsOpenModalAvailable] = useState(false);
+
+  const openModalWallet = () => {
+    setIsOpenModalWallet(true);
+  };
+
+  const closeModalWallet = () => {
+    setIsOpenModalWallet(false);
+  };
+
+  const openModalAvailable = () => {
+    setIsOpenModalAvailable(true);
+  };
+
+  const closeModalAvailable = () => {
+    setIsOpenModalAvailable(false);
+  };
   return (
     <div className="flex items-center bg-[#F4F2F2] h-[300px] w-[100vw] px-60">
       <div className="flex items-center justify-between h-[250px] w-[1000px]">
@@ -46,15 +67,29 @@ function Bio() {
             rate: 10,000 THB/hr
           </div>
           <div className="flex flex-row gap-[20px]">
-            <div className="flex flex-row justify-center items-center bg-[#9AC0B5] text-white font-bold rounded-[20px] w-[180px] h-[60px]">
+            <div
+              onClick={openModalWallet}
+              className="flex flex-row justify-center items-center bg-[#9AC0B5] text-white font-bold rounded-[20px] w-[180px] h-[60px]"
+            >
               Wallet: 50,000 THB
             </div>
-            <div className="flex flex-row justify-center items-center bg-[#9AC0B5] text-white font-bold rounded-[20px] w-[140px] h-[60px]">
+            <div
+              onClick={openModalAvailable}
+              className="flex flex-row justify-center items-center bg-[#9AC0B5] text-white font-bold rounded-[20px] w-[140px] h-[60px]"
+            >
               Availability
             </div>
           </div>
         </div>
       </div>
+      <ModalWallet
+        isOpenModalWallet={isOpenModalWallet}
+        closeModalWallet={closeModalWallet}
+      />
+      <ModalAvailable
+        isOpenModalAvailable={isOpenModalAvailable}
+        closeModalAvailable={closeModalAvailable}
+      />
     </div>
   );
 }
