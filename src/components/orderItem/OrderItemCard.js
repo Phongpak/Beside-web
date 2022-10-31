f361480c1701e54db44745fe60129a1d4d5f1a9a
natikawrs <natika.wrs@gmail.com>
2022-10-31 15:15:52 +0700

    adminOrderItem

--- src/components/orderItem/OrderItemCard.js
+++ src/components/orderItem/OrderItemCard.js
@@ -1,17 +1,17 @@
 import React from "react";
+import moment from "moment";
 
-function OrderItemCard() {
+function OrderItemCard({ item }) {
+  // console.log(item);
   return (
     <>
-      <div className="flex flex-row justify-center items-center min-w-[1056px]  border-4 border-[#9AC0B5] rounded-[15px] p-5">
-        <div className="flex  items-center h-[85%]">
-          <img
-            className="w-1/3"
-            src="https://owi.ucdavis.edu/sites/g/files/dgvnsk8926/files/styles/sf_landscape_16x9/public/media/images/google-maps-satellite.png?h=83823e20&itok=G9S9rzyI"
-          />
+      <div className="flex flex-row justify-between items-center min-w-[1056px]  border-4 border-[#9AC0B5] rounded-[15px] p-5">
+        {/* <div className="flex  items-center h-[85%] justify-between"> */}
+        <div className=" border-4 border-[#9AC0B5]  w-60 h-60 justify-between "></div>
 
-          <div className="flex flex-row gap-20 h-[100%] text-[#224957] ml-40">
-            <div className="flex flex-col justify-between  font-medium gap-5">
+        <div>
+          <div className="flex flex-row gap-20 h-[100%] text-[#224957] ">
+            <div className="flex flex-col  font-medium gap-5 ">
               <div>Customer:</div>
               <div>Provider:</div>
               <div>Date:</div>
@@ -19,31 +19,48 @@ function OrderItemCard() {
               <div>Location:</div>
               <div>Deal Price:</div>
             </div>
-            <div className="flex flex-col justify-between  font-medium gap-5">
-              <div>wowwww@gmail.com</div>
-              <div>wowwww@gmail.com</div>
-              <div>Monday 10 September 2022</div>
-              <div>10:34 am. </div>
-              <div>Siam Paragon </div>
-              <div>10,000 THB </div>
+            <div className="flex flex-col   font-medium gap-5 w-96">
+              <div>{item.customer.email}</div>
+              <div>{item.provider.email}</div>
+              <div>
+                {" "}
+                {moment(item.appointmentDate).format("dddd, MMMM Do YYYY")}
+              </div>
+              <div className="">
+                {item.fromTime.slice(0, 5)} - {item.toTime.slice(0, 5)}
+              </div>
+              <div>{item.location} </div>
+              <div>{item.rentPriceTotal} THB </div>
             </div>
-          </div>
-          <div className=" ">
-            <div className="text-[#224957] justify-center items-center p-3 border-2 border-[#E6C3C1] rounded-[20px] ">
-              Pending Confirmation
-            </div>
-            <div className="justify-center items-center p-3 rounded-[20px] bg-[#E6C3C1] text-[#224957] ">
-              Denied
-            </div>
-            <div className="text-[#224957] justify-center items-center p-3 border-2 border-[#9AC0B5] rounded-[20px]">
-              Upcomming
-            </div>
-            <div className="justify-center items-center p-3 rounded-[20px] bg-[#9AC0B5] text-[#224957]">
-              Success
+            <div className="flex self-end gap-5 w-60 justify-end">
+              {item.status === "PENDING" && (
+                <div className="text-[#224957] justify-center items-center p-3 border-2 border-[#E6C3C1] rounded-[20px] ">
+                  Pending Confirmation
+                </div>
+              )}
+
+              {item.status === "REJECT" && (
+                <div className="justify-center items-center p-3 rounded-[20px] bg-[#E6C3C1] text-[#224957] ">
+                  Rejected
+                </div>
+              )}
+
+              {item.status === "INPROGRESS" && (
+                <div className="text-[#224957] justify-center items-center p-3 border-2 border-[#9AC0B5] rounded-[20px]">
+                  Upcomming
+                </div>
+              )}
+
+              {item.status === "SUCCESS" && (
+                <div className="justify-center items-center p-3 rounded-[20px] bg-[#9AC0B5] text-[#224957]">
+                  Success
+                </div>
+              )}
             </div>
           </div>
         </div>
       </div>
+      {/* </div> */}
     </>
   );
 }
